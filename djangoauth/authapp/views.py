0cd989c7dc29e00ab801cb53583b3cf53cbd9a87
Saurabh Sood <saurabhsood91@gmail.com>
2016-10-27 19:42:58 -0700

    better form handling

--- djangoauth/authapp/views.py
+++ djangoauth/authapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from forms import CreateUserForm
 from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.models import User
 
 def create_user_view(request):
     if request.method == 'GET':
@@ -10,16 +11,34 @@ def create_user_view(request):
         form = CreateUserForm(request.POST)
 
         if form.is_valid():
+            email = form.cleaned_data['email_address']
             password = form.cleaned_data['password']
             confirm_password = form.cleaned_data['password_repeat']
 
             if password == confirm_password:
-                return JsonResponse({
-                    'data': 1
-                })
+                # check if user exists
+                user = User.objects.filter(username=email).first()
+                if not user:
+                    new_user = User.objects.create_user(
+                        username=email,
+                        email=email,
+                        password=password
+                    )
+                    new_user.save()
+                    return JsonResponse({
+                        'data': 1
+                    })
+                else:
+                    return JsonResponse({
+                        'data': -1
+                    })
             else:
-                return JsonResponse({
-                    'data': 0
+                error = {
+                    'msg': 'Passwords are not the same'
+                }
+                return render(request, 'create_user.html', {
+                    'form': form,
+                    'error': error
                 })
 
 
